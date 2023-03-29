cedf4cd094c5fa3eabf7ff8b13b8c1fe7418a83f
snoopyujin <128570672+snoopyujin@users.noreply.github.com>
2023-03-29 23:18:42 +0900

    Add files via upload

---
+++ 11382.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long c = sc.nextLong();
+		
+		System.out.println(a + b + c);
+		
+	}
+
+}
